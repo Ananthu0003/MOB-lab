bd57aa21101191c3c5862d2b2821798347795786
Anantha Padmanabhan <118457510+Ananthu0003@users.noreply.github.com>
2024-08-19 01:38:33 -0700

    Create login.java

---
+++ login.java
@@ -0,0 +1,48 @@
+package com.example.anaghalogin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+EditText e1,e2;
+Button b;
+String s1,s2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.e1) ;
+        e2=(EditText)findViewById(R.id.e2) ;
+        b=(Button)findViewById(R.id.b1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1=e1.getText().toString();
+                s2=e2.getText().toString();
+                if(s1.equals("admin") && s2.equals("pass"))
+                {
+                    Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"login Failed",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
